7b8880ce023c7c9f7367c0d502b6c43dd47bfce1
Farzeen <happycoder97@protonmail.com>
2021-01-11 10:04:03 +0530

    Downloader script and Makefile

---
+++ download_myexpos.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function download() {
+    component="$1"
+    echo "Downloading $component .."
+    wget "https://github.com/eXpOSNitc/$component/archive/master.zip" -c -q -O "$component.zip"
+    unzip -q "$component".zip
+    mv "$component"-master "$component"
+    rm "$component".zip
+    echo "$component downloaded."
+}
+
+download expl
+download spl
+download xfs-interface
+download xsm
